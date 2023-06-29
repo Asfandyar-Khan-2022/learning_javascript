e7cb4cdd7e9daf46dfdae7eb46b9ff2734c1f717
Asfandyar-Khan-2022 <117299102+Asfandyar-Khan-2022@users.noreply.github.com>
2023-06-29 08:04:35 +0100

    02 lesson complete

--- javascript_full_course/02_lesson/js/main.js
+++ javascript_full_course/02_lesson/js/main.js
@@ -1,4 +1,17 @@
-console.log('Hello World')
-console.log(typeof 'Asfandyar');
-console.log(typeof 42);
-console.log(typeof true);
+// Strings
+const myVariable = 'Mathematics';
+
+// The length property
+// console.log('Every good boydoes fine'.length);
+
+// String Methods
+// console.log(myVariable.charAt(5));
+// console.log(myVariable.indexOf('at'));
+// console.log(myVariable.lastIndexOf('at'));
+// console.log(myVariable.slice(5, 8));
+// console.log(myVariable.toUpperCase());
+// console.log(myVariable.toLowerCase());
+// console.log(myVariable.includes('mat'));
+// console.log(myVariable.split('e'));
+console.log('John, Joe, Dave'.split(','));
+console.log('Every good boy does fine'.split(' '));
