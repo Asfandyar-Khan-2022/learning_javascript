89cf47aa7933733bdbbeb3ac9a23993b310ee11d
Asfandyar-Khan-2022 <117299102+Asfandyar-Khan-2022@users.noreply.github.com>
2023-07-02 08:37:50 +0100

    11 lesson complete

---
+++ javascript_full_course/11_lesson/js/main.js
@@ -0,0 +1,22 @@
+// Javascript Classes
+class Pizza {
+    crust = "orignal";
+    #sauce = "traditional";
+    #size;
+    constructor(pizzaSize) {
+        this.#size = pizzaSize;
+    }
+    getCrust() {
+        return this.crust;
+    }
+    setCrust(pizzaCrust) {
+        this.crust = pizzaCrust;
+    }
+    hereYouGo() {
+        console.log(`Here's your ${this.crust} ${this.#sauce} sauce ${this.#size} pizza.`)
+    }
+}
+
+const myPizza = new Pizza("large");
+myPizza.hereYouGo();
+console.log(myPizza.crust);
