8dd2b4f316e0bc3792d288147e38cc877cc1e391
Asfandyar-Khan-2022 <117299102+Asfandyar-Khan-2022@users.noreply.github.com>
2023-07-02 10:21:22 +0100

    13 lesson

---
+++ javascript_full_course/12_lesson/js/main.js
@@ -0,0 +1,27 @@
+// JSON: JavaScript Object Notation
+/*
+JSON is sued to send and receive data.
+JSON is a text format that is completely language independed.
+Meaning JSON is used to send & receive data in many languages
+...not just in JavaScript.
+*/
+
+const myObj = {
+    name: "Asfandyar",
+    hobbies: ["eat", "sleep", "code"],
+    hello: function (){
+        console.log("Hello!");
+    }
+};
+console.log(myObj);
+console.log(myObj.name);
+myObj.hello();
+console.log(typeof myObj);
+
+const sendJSON = JSON.stringify(myObj);
+console.log(sendJSON)
+console.log(typeof sendJSON);
+
+const receiveJSON = JSON.parse(sendJSON);
+console.log(receiveJSON);
+console.log(typeof receiveJSON)
